4f269ef2d1d3f8fdc14651c8ccc168925a5465d3
Abdur Rafay <cornal008@gmail.com>
2023-06-07 04:37:19 +0500

    init portfolio

---
+++ src/app/api/route.tsx
@@ -0,0 +1,13 @@
+import { NextResponse } from "next/server";
+
+
+export async function GET(request: Request) {
+    const requestOptions = {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' },
+    }
+    const resp = await fetch('https://api.ipify.org?format=json', requestOptions);
+    const data = await resp.json();
+
+    return NextResponse.json({ data });
+}
---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+
+
+
+export default function Page() {
+    
+    
+    return (
+        <>
+            Hello world 232
+        </>
+    )
+}
